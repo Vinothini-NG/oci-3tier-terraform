aa4f0e6b9f3515a4dc5de5b70ac14a9f3a260372
Vino <nvinothin21@gmail.com>
2026-06-02 14:26:59 +0530

    untill creation of ap1

--- network.tf
+++ network.tf
@@ -6,3 +6,16 @@ resource "oci_core_vcn" "main_vcn" {
   dns_label      = "main"
 }
 
+#CREATE PUBLIC-ROUTE-TABLE FOR GATEWAY
+resource "oci_core_route_table" "public_rt" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.main_vcn.id
+  display_name   = "publicRT-tf"
+
+  # ADD RULES FOR THAT PUBLIC-ROUTE-TABLE
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.main_igw.id
+  }
+}
